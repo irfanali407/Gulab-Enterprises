be5809aa7219a305fbdfd6e70b31f54d2754bcdb
irfan <aliirfan2503@gmail.com>
2026-09-17 01:49:55 +0530

    second

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'https://gulab-enterprises-2.onrender.com';
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
