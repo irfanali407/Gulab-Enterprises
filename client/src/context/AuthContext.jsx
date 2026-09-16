81280f2f5749bce2556faba6203957ed519c885b
irfan <aliirfan2503@gmail.com>
2026-09-17 02:18:50 +0530

    commit6

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'https://gulab-enterprises-2.onrender.com';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://gulab-enterprises-2.onrender.com';
 
 const AuthContext = createContext();
 
